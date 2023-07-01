c4492a6df81b77e911d927c95767ca658c6c4d0b
SanjayAppari <apparisanjaym6@gmail.com>
2023-07-01 06:33:56 +0530

    Day2 started

--- src/components/DisplayItems.js
+++ src/components/DisplayItems.js
@@ -1,25 +1,47 @@
 import React, { useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import ItemCard from './ItemCard';
 
 function DisplayItems() {
-  const {name} = useParams();
+  const { name } = useParams();
 
-  useEffect(()=>{
+  useEffect(() => {
 
-  },[]);
+  }, []);
   return (
-    <div className='displayItems' style={{marginTop:'50px'}}>
-         <h3>All {name.charAt(0).toUpperCase()+name.slice(1)}</h3>
-         <div className="container">
-            <ItemCard/>
-            <ItemCard/>
-            <ItemCard/>
-            <ItemCard/>
-            <ItemCard/>
-            <ItemCard/>
-            <ItemCard/>
-         </div>
+    <div className='displayItems' style={{ marginTop: '50px' }}>
+      <h3>All {name.charAt(0).toUpperCase() + name.slice(1)}</h3>
+      <div className="filter">
+          <li class="nav-item dropdown list-unstyled mx-2">
+            <Link class="nav-link  active" to="/all/clothing" aria-expanded="">
+              Gender  <i class="fa-sharp fa-solid fa-caret-down"></i>
+            </Link>
+            <ul class="dropdown-menu">
+              <li><Link class="dropdown-item  bg-transparent" to="/">Male</Link></li>
+              <li><hr class="dropdown-divider text-white" /></li>
+              <li><Link class="dropdown-item  bg-transparent" to="/">Female</Link></li>
+            </ul>
+          </li>
+          <li class="nav-item dropdown list-unstyled mx-2">
+            <Link class="nav-link  active" to="/all/clothing" aria-expanded="">
+              Brand  <i class="fa-sharp fa-solid fa-caret-down"></i>
+            </Link>
+            <ul class="dropdown-menu">
+              <li><Link class="dropdown-item  bg-transparent" to="/">Male</Link></li>
+              <li><hr class="dropdown-divider text-white" /></li>
+              <li><Link class="dropdown-item  bg-transparent" to="/">Female</Link></li>
+            </ul>
+          </li>
+      </div>
+      <div className="container">
+        <ItemCard />
+        <ItemCard />
+        <ItemCard />
+        <ItemCard />
+        <ItemCard />
+        <ItemCard />
+        <ItemCard />
+      </div>
     </div>
   )
 }
